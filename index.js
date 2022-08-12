c5f8c9de470c2336c0c39263f55af49d630f5e97
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2022-08-12 06:04:43 +0000

    Day1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // app.set('views', path.join(__dirname + 'views'))
@@ -8,7 +11,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '/views/index.html'));
+    res.status(200).sendFile(path.join(__dirname, '/views/Login_Page/index.html'));
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.sendFile(path.join(__dirname, './views/Login_Status/MainPage.html'));
 });
 
 app.get('/*', (req, res) => {
